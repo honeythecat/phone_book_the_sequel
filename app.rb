299cb29363f8e5eea9587e6d6b974cbf94faeaaa
Heather <heather@thisisnotanorg.org>
2015-02-04 20:19:56 +0000

    Add index.erb and form.

--- app.rb
+++ app.rb
@@ -1,5 +1,20 @@
-require('sinatra')
-require('sinatra/reloader')
-also_reload('lib/**/*.rb')
-require('./lib/contact.rb')
+require 'sinatra'
+require 'sinatra/reloader'
+also_reload 'lib/**/*.rb'
+require './lib/contact'
+require './lib/phone'
+set :bind, '0.0.0.0'
+set :port, 3000
+
+get '/' do
+	@contacts = Contact.all()
+	erb :index
+end
+
+post '/' do
+	name = params["name"]
+	Contact.new(name).save()
+	@contacts = Contact.all()
+	erb :index
+end
 
